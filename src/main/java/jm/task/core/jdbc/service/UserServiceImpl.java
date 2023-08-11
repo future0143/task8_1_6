39fccdaf90c4c79d827d33bbd61d901c280bec95
future0143 <125833563+future0143@users.noreply.github.com>
2023-08-11 21:19:29 +0200

    Update UserServiceImpl.java
    добавлены аннотации @Override

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -10,24 +10,24 @@ import java.util.List;
 
 public class UserServiceImpl extends Util implements UserService {
     private UserDao userDaoJDBC = new UserDaoJDBCImpl();
-
+    @Override
     public void createUsersTable() throws SQLException {
         userDaoJDBC.createUsersTable();
     }
-
+    @Override
     public void dropUsersTable() throws SQLException {
         userDaoJDBC.dropUsersTable();
     }
-
+    @Override
     public void saveUser(String name, String lastName, byte age) throws SQLException {
         userDaoJDBC.saveUser(name,lastName,age);
         System.out.println("User с именем " + name + "добавлен в базу данных");
     }
-
+    @Override
     public void removeUserById(long id) throws SQLException {
         userDaoJDBC.removeUserById(id);
     }
-
+    @Override
     public List<User> getAllUsers() throws SQLException {
         List<User> users =  userDaoJDBC.getAllUsers();
         for (User user : users) {
@@ -35,7 +35,7 @@ public class UserServiceImpl extends Util implements UserService {
         }
         return users;
     }
-
+    @Override
     public void cleanUsersTable() throws SQLException {
         userDaoJDBC.cleanUsersTable();
     }
